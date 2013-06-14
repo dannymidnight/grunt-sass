8c118912ba1f3e6ee1710aa2f01f63cd3d6fcc40
Chris Campbell <chris@harto.org>
2013-06-14 17:14:25 +1000

    Optionally output files to different destination

--- tasks/sass.js
+++ tasks/sass.js
@@ -5,14 +5,16 @@ module.exports = function (grunt) {
 	grunt.registerMultiTask('sass', 'Compile SCSS to CSS', function () {
 		var options = this.options({
 			includePaths: [],
-			outputStyle: 'nested'
+			outputStyle: 'nested',
+			dest: null
 		});
 
 		grunt.util.async.forEachSeries(this.files, function (el, next) {
 			sass.render({
 				file: el.src[0],
 				success: function (css) {
-					grunt.file.write(el.dest, css);
+					var destination = options.dest || el.dest;
+					grunt.file.write(destination, css);
 					next();
 				},
 				error: function (err) {
